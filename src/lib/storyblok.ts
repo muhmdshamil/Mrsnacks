440129c7b6fc3428be0a078a4909b842b2d66866
muhmdshamil <shamiilmuhmd@gmail.com>
2026-03-08 14:29:58 +0530

    added.env

--- src/lib/storyblok.ts
+++ src/lib/storyblok.ts
@@ -2,7 +2,7 @@ import { storyblokInit, apiPlugin } from "@storyblok/react/rsc";
 import Products from "@/components/Products";
 
 export const getStoryblokApi = storyblokInit({
-    accessToken: "891nvEKD0scXWZkXMg0myQtt",
+    accessToken: process.env.STORYBLOK_TOKEN || "891nvEKD0scXWZkXMg0myQtt",
     use: [apiPlugin],
     components: {
         Products: Products,
